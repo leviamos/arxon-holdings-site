725882a77d8e4a881512504a4adb13658baa3b1c
leviamos <leviamosrobinson@gmail.com>
2025-11-08 01:37:34 +1100

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: [
+    "./src/app/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        arxon: {
+          bg: "#020817",
+          accent: "#38bdf8",
+          accentSoft: "#0f172a"
+        }
+      }
+    }
+  },
+  plugins: [],
+};
